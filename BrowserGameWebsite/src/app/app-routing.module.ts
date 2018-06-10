de362eed83e2da23eb76ce6df1c7366606321fdc
Oolious <37305815+Oolious@users.noreply.github.com>
2018-06-09 22:05:31 -0400

    Added a few comments to help document the code I added

--- BrowserGameWebsite/src/app/app-routing.module.ts
+++ BrowserGameWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
+//This module will define the different pages of the website
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 
+//This array ties components to specific paths
 const routes: Routes = [
   { path: 'about', component: AboutComponent }
 ];
